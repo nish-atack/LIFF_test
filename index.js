e6395e8b53934a692f1897221de576063ff20459
nish-atack <106975240+nish-atack@users.noreply.github.com>
2022-06-14 16:26:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ $(function () {
         var reasons = $('textarea[name="reasons"]').val();
         var time1 = $('input[name="time1"]').val();
         var num1 = $('input[name="num1"]').val();
+        var breed = $('input[name="breed"]').val();
         
         // var date = $('input[name="date"]').val();
         // var tool = $('input[name="tool"]').val();
         
         //var msg = `${key}\n朝礼/終礼不参加の理由：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
-        var msg = `${key}\n理由：${reasons} \n日時：${time1}\n数：${num1}`;
+        var msg = `${key}\n種類：${} \n日時：${time1}\n数：${num1}`;
         sendText(msg);
 
         return false;
