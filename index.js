092ceba6abe892e2bf1ec81ef657ff3031184c7e
nish-atack <106975240+nish-atack@users.noreply.github.com>
2022-06-21 01:16:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ var key = params.get('key');
 $(function () {
     // 送信
     $('form').submit(function () {
-        var reasons = $('textarea[name="reasons"]').val();
-        var time1 = $('input[name="time1"]').val();
-        var num1 = $('input[name="num1"]').val();
-        var breed = $('input[name="breed"]').val();
+     
+        //var time0 = $('input[name="time1"]').val();
+        //var num0 = $('input[name="num1"]').val();
+        //var breed0 = $('input[name="breed"]').val();
+        
+        var breed0 = obj.filter(function(input) {
+  return input.name.match(/breed/);
+});
+        
         
         // var date = $('input[name="date"]').val();
         // var tool = $('input[name="tool"]').val();
-        
+          // var reasons0 = $('textarea[name="reasons"]').val();
         //var msg = `${key}\n朝礼/終礼不参加の理由：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
-    var msg = `ご注文内容：\n${breed1} \n${num1} \n${tim1} \n${reasons1}`;
+    var msg = `ご注文内容：\n${breed0} \n${num0} \n${tim0} \n${reasons0}`;
         
-    var msg = `ご注文内容：\n${breed2} \n${num2} \n${tim2} \n${reasons2}`;
         
         sendText(msg);
 
